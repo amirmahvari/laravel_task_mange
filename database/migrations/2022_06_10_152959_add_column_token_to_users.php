8830a143af1a215db217f815813a4eac39e3ef74
Amir Abbas <amirabbas750@gmail.com>
2022-06-10 20:25:59 +0430

    user factory

--- database/migrations/2022_06_10_152959_add_column_token_to_users.php
+++ database/migrations/2022_06_10_152959_add_column_token_to_users.php
@@ -14,7 +14,7 @@ class AddColumnTokenToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('token','255')->after('password');
+            $table->string('token','255')->nullable()->after('password');
         });
     }
 
